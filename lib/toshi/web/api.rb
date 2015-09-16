d5eef3dafbe68cab7a09f3dd87523d6847a99f49
George Millo <georgejulianmillo@gmail.com>
2015-09-16 14:58:20 +0200

    check that params[:hash] is numeric

--- lib/toshi/web/api.rb
+++ lib/toshi/web/api.rb
@@ -273,7 +273,7 @@ module Toshi
       def load_block
         block = if params[:hash].to_s == 'latest'
                   Models::Block.head
-                elsif params[:hash].to_s.size < 64
+                elsif params[:hash].to_s =~ /\A\d{,64}\z/
                   Models::Block.find(height: params[:hash], branch: 0)
                 else
                   Models::Block.find(hsh: params[:hash])
